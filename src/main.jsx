7f04e17e9ca887ff0944ec3518c906a50c85b7a8
Gustavo Souza <gustavovbs270@gmail.com>
2022-12-27 13:18:16 -0300

    Learn about folder structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import Home from './pages/Home';
 
 ReactDOM.createRoot(document.querySelector('#root'))
 .render(
   <React.StrictMode>
-    <App/>
+    <Home/>
   </React.StrictMode>,
 )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div className="App">
+            <h1>olá</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis asperiores recusandae impedit quasi voluptas quos nemo quia iure laboriosam. Reiciendis quam facilis animi vero. Placeat, doloremque. Eum cupiditate amet assumenda.</p>
+        </div>
+    );
+}
+
+export default Home;
