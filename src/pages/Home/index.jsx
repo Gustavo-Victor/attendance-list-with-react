3d2f750f397bc111fa093fbdc48121b128169466
Gustavo Souza <gustavovbs270@gmail.com>
2022-12-27 15:37:36 -0300

    Learn about State

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,24 @@
+//imports
 import './style.css';
 import {Card} from '../../components/Card';
+import {useState} from 'react';
 
+//component
 export function Home() {
+    //state
+    const [name, setName] = useState(""); 
+
+    //content
     return (
         <div className='container'>
-            <h1>Lista de Presença</h1>
-            <input type="text" placeholder="Digite o nome..." />
+            <h1>Nome: {name}</h1>
+            <input 
+            type="text"
+            placeholder="Digite o nome..."
+            onChange={e => {setName(e.target.value)}} 
+            />
             <button type="button">Enviar</button>
-        
+
             <Card name="Rodrigo" time="10:55:25" />
             <Card name="Juliana" time="10:56:46" />
         </div>
