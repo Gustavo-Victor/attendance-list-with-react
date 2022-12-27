1f588a62812186899b2059cd952caa0527f3ad80
Gustavo Souza <gustavovbs270@gmail.com>
2022-12-27 12:14:55 -0300

    Learn about JSX

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.querySelector('#root'))
+.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
 )
