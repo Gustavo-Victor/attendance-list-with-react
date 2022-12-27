e1136ff74c909e43bde897601fa248e4bcc82609
Gustavo Souza <gustavovbs270@gmail.com>
2022-12-27 13:47:38 -0300

    Use css files

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './pages/Home';
+import './styles/global.css';
 
 ReactDOM.createRoot(document.querySelector('#root'))
 .render(
